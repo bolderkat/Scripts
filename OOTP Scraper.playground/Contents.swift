6bce0e022c8b7dc49e1b8b13c08412dba01c616b
Daniel Luo <d.luo117@gmail.com>
2021-03-11 20:39:36 -0800

    Scrape webpage, filter for completed games

--- OOTP Scraper.playground/Contents.swift
+++ OOTP Scraper.playground/Contents.swift
@@ -1,3 +1,13 @@
-import UIKit
+import Foundation
+let url = URL(string: "http://2020ele.com/game/lgreports/real_time_sim/")
+var htmlString = ""
 
-var str = "Hello, playground"
+do {
+    htmlString = try String(contentsOf: url!, encoding: .ascii)
+} catch let error {
+    print("Error: \(error)")
+}
+let games = htmlString.components(separatedBy: #"<table width="474px" cellspacing="4" cellpadding="0" style="border:1px solid #F1FFFF; background-color:#626E64; margin-bottom:8px;">"#)
+
+let finishedGames = games.filter {$0.contains("FINAL") }
+finishedGames.count
