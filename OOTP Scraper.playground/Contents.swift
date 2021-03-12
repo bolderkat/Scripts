0cfc2433b48989a72cbe2c8336f748784b76a6d7
Daniel Luo <d.luo117@gmail.com>
2021-03-11 23:58:56 -0800

    Parse HTML into discrete game results with scores, etc.

--- OOTP Scraper.playground/Contents.swift
+++ OOTP Scraper.playground/Contents.swift
@@ -19,30 +19,34 @@ var games = htmlString.components(separatedBy: #"<table width="474px" cellspacin
 games.removeFirst()
 
 
-let finishedGames = games.filter {$0.contains("FINAL") }
+let finishedGames = games.filter { $0.contains("FINAL") }
 
 for game in finishedGames {
-    var teamResults = game.components(separatedBy: #"<img src="../images/team_logos/"#)
-    teamResults.removeFirst()
-    for result in teamResults {
-        if let teamIndex = result.index(of: "_16") {
-            let subString = result[..<teamIndex]
+    var results: [TeamResult] = []
+    var teamResultString = game.components(separatedBy: #"<img src="../images/team_logos/"#)
+    teamResultString.removeFirst()
+    for resultString in teamResultString {
+        if let teamIndex = resultString.index(of: "_16") {
+            let subString = resultString[..<teamIndex]
             let teamString = String(subString)
             
             // R, H, and E values lie between these HTML tags
-            let resultStartIndexes = result.endIndices(of: #"<span style="color:#F6EF7D;">"#)
-            let resultEndIndexes = result.indices(of: "</span></td>")
+            let resultStartIndexes = resultString.endIndices(of: #"<span style="color:#F6EF7D;">"#)
+            let resultEndIndexes = resultString.indices(of: "</span></td>")
             
-            let runs = result[resultStartIndexes[0]...resultEndIndexes[0]].dropLast()
-            let hits = result[resultStartIndexes[1]...resultEndIndexes[1]].dropLast()
-            let errors = result[resultStartIndexes[2]...resultEndIndexes[2]].dropLast()
-            if let team = Team(rawValue: teamString) {
-                print("\(team) --- Runs: \(runs), hits: \(hits), Errors: \(errors)")
-                
+           
+            
+            if let team = Team(rawValue: teamString),
+               let runs = Int(resultString[resultStartIndexes[0]...resultEndIndexes[0]].dropLast()),
+               let hits = Int(resultString[resultStartIndexes[1]...resultEndIndexes[1]].dropLast()),
+               let errors = Int(resultString[resultStartIndexes[2]...resultEndIndexes[2]].dropLast()) {
+                let result = TeamResult(team: team, runs: runs, hits: hits, errors: errors)
+                results.append(result)
             }
         }
     }
-    
+    let game = FinishedGame(awayResult: results[0], homeResult: results[1])
+    print("\(game.winner.name) beat the \(game.loser.name), \(game.awayResult.runs)-\(game.homeResult.runs)")
 }
 
 
